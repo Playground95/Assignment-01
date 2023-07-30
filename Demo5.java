fea6a345792cc80905f37c0c4833f619a7f4b893
nadeesha <nadeeshalakshan525@gmail.com>
2023-07-30 11:01:38 +0530

    Complete Task 5

---
+++ Demo5.java
@@ -0,0 +1,12 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        
+        int prefix = 978;
+        int group = 3;
+        int publisher = 16;
+        int title = 1484100;
+
+        System.out.printf("\"\033[31m%d-%d-%d\033[0m-%d\"", prefix, group, publisher, title);
+    }
+    
+}

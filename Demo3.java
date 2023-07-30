9eaa9baa654136455dd499dfc041b86aa0d89f48
nadeesha <nadeeshalakshan525@gmail.com>
2023-07-30 09:20:33 +0530

    Complete Task 3

---
+++ Demo3.java
@@ -0,0 +1,10 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        
+        int numInt1 = 123;
+        int numInt2 = 456;
+        int numInt3 = 7890;
+
+        System.out.printf("\033[34m(%d)\033[0m %d-%d", numInt1, numInt2, numInt3);
+    }
+}
